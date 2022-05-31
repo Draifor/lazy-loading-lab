6ce31645ee32ee9ef18f65b727b5104b6e7a13e1
Alfredo Parra <alfredo-920@hotmail.com>
2022-05-31 10:33:32 -0500

    Lazy was implemented in App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import Nav from './components/Nav/Nav';
-import Card from './components/Card/Card';
-import Home from './pages/Home/Home';
-import Contact from './pages/Contact/Contact';
-import About from './pages/About/About';
-import NotFound from './pages/NotFound/NotFound';
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import Card from "./components/Card/Card";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Contact = lazy(() => import("./pages/Contact/Contact"));
+const About = lazy(() => import("./pages/About/About"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"));
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Nav/>
+        <Nav />
         <Card>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/home' element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<h2>Cargando...</h2>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </Card>
       </div>
     </Router>
